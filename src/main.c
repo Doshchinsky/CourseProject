1422857d880b46f143a4f784017760decc49946a
WhoAmI <akter1997@gmail.com>
2016-05-28 21:21:48 +0600

    Added copying from input file by words

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,6 +18,21 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "CourseProject: An error has occured!\nERROR: Program can't open file '%s' for reading!\nHint: Please, check your file!\n", filename);
 		exit(EXIT_FAILURE);
 	}
+
+	int height = 1024, length = 1024, amount_of_words = 0;
+	
+	char **strings = (char **)malloc(height * sizeof(char *));
+	char *input_word = (char *)malloc(length * sizeof(char));
+
+	while (!feof(input_file)) {
+		fscanf(input_file, "%s", input_word);
+		strings[amount_of_words] = malloc(sizeof(input_word));
+		strings[amount_of_words] = strdup(input_word);
+		printf("String #%d -> %s\n", amount_of_words + 1, strings[amount_of_words]);
+		amount_of_words++;
+	}
+	fclose(input_file);
+	free(strings);
 
 	return EXIT_SUCCESS;
 }
